69806431a3fe511762e19058d9d1688777559db8
askiad <askiad@stanford.edu>
2021-07-20 01:37:44 -0700

    simple priority experiment local variables

--- k8s-scheduler/src/main/java/com/vmware/dcm/KubernetesLocalExpr.java
+++ k8s-scheduler/src/main/java/com/vmware/dcm/KubernetesLocalExpr.java
@@ -35,13 +35,6 @@ public class KubernetesLocalExpr {
     private final ListeningScheduledExecutorService scheduledExecutorService =
             MoreExecutors.listeningDecorator(Executors.newScheduledThreadPool(100));
 
-    private int podsPerPriorityLevel = 5;
-    private int pr1StartTimeSec = 0;
-    private int pr2StartTimeSec = 4;
-    private int pr2EndTimeSec = 30;
-    private int pr1EndTimeSec = 40;
-    private int waitTimeSec = 20;
-
     public void run(final NamespacedKubernetesClient client, final String configFileName, final IPodDeployer deployer)
             throws Exception {
         // TODO: rich log info
@@ -52,21 +45,19 @@ public class KubernetesLocalExpr {
         try (final InputStream inputStream = getClass().getClassLoader().getResourceAsStream(configFileName)) {
             final Yaml yaml = new Yaml();
             final Map<String, Object> config = yaml.load(inputStream);
+            final int podsPerPriorityLevel = (int) config.get("podsPerPriorityLevel");
+            final int pr1StartTimeSec = (int) config.get("pr1StartTimeSec");
+            final int pr2StartTimeSec = (int) config.get("pr2StartTimeSec");
+            final int pr2EndTimeSec = (int) config.get("pr2EndTimeSec");
+            final int pr1EndTimeSec = (int) config.get("pr1EndTimeSec");
+            final int waitTimeSec = (int) config.get("waitTimeSec");
 
-            podsPerPriorityLevel = (int) config.get("podsPerPriorityLevel");
-            pr1StartTimeSec = (int) config.get("pr1StartTimeSec");
-            pr2StartTimeSec = (int) config.get("pr2StartTimeSec");
-            pr2EndTimeSec = (int) config.get("pr2EndTimeSec");
-            pr1EndTimeSec = (int) config.get("pr1EndTimeSec");
-            waitTimeSec = (int) config.get("waitTimeSec");
-
-
-    final List<Pod> deployment1 = getDeployment(client, "priority-level1");
+            final List<Pod> deployment1 = getDeployment(client, "priority-level1", podsPerPriorityLevel);
             // create deployment in the k8s cluster at the correct start time
             final ListenableFuture<?> scheduledStart1 = scheduledExecutorService.schedule(
                     deployer.startDeployment(deployment1), pr1StartTimeSec, TimeUnit.SECONDS);
 
-            final List<Pod> deployment2 = getDeployment(client, "priority-level2");
+            final List<Pod> deployment2 = getDeployment(client, "priority-level2", podsPerPriorityLevel);
             // create deployment in the k8s cluster at the correct start time
             final ListenableFuture<?> scheduledStart2 = scheduledExecutorService.schedule(
                     deployer.startDeployment(deployment2), pr2StartTimeSec, TimeUnit.SECONDS);
@@ -88,20 +79,19 @@ public class KubernetesLocalExpr {
                     deletion.addListener(() -> onComplete1.set(true), scheduledExecutorService);
                 }, scheduledExecutorService);
             deletions.add(onComplete1);
-            
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-        }
 
-        
         final int exprTimeSec = (int) System.currentTimeMillis() * 1000 - startTimeSec;
         final List<Object> objects = Futures.successfulAsList(deletions)
             .get(pr2EndTimeSec + waitTimeSec + exprTimeSec, TimeUnit.SECONDS);
+            assert objects.size() != 0;
 
-        assert objects.size() != 0;
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private List<Pod> getDeployment(final NamespacedKubernetesClient client, final String priority) {
+    private List<Pod> getDeployment(final NamespacedKubernetesClient client, final String priority,
+                                    final int podsPerPriorityLevel) {
         // Load the template file and update its contents to generate a new deployment template
         final String podFile = "priority-test/pod-nodeaf-nodeantiaf.yml";
         return IntStream.range(0, podsPerPriorityLevel)
